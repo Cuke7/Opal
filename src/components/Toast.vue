0340a0df7aa169bf1b680cd3e5f1ff7da08ebb43
Cuke7 <cuke.dev@mail.com>
2022-10-16 17:35:42 +0200

    lg text for toast

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="toast toast-bottom">
-        <div class="bg-neutral font-mono text-neutral-content font-bold p-4 rounded-lg text-2xl border-primary border-2">
+        <div class="bg-neutral font-mono text-neutral-content font-bold p-4 rounded-lg text-lg border-primary border-2">
             <div>
                 <span>{{ text }}</span>
             </div>
