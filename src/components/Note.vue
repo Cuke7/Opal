f01cd858891a65eb1adaa8d5ae94b64c554d75e9
Cuke7 <cuke.dev@mail.com>
2022-10-20 20:42:20 +0200

    tset

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -2,11 +2,11 @@
     <div class="flex h-full bg-base-100 text-base-content p-4 sm:px-8">
         <div ref="editor" class="h-full sm:w-1/2 sm:flex flex-col w-full flex hidden" v-if="!viewOnly">
             <ToolBar @toggle="toggle" :view="view" class="w-full lg:w-1/2" />
-            <textarea @blur="saveNote" v-model="note.text" class="flex-1 bg-transparent w-full overflow-y-auto p-8 pt-4 lg:text-lg"> </textarea>
+            <textarea @blur="saveNote" v-model="note.text" class="flex-1 bg-transparent w-full overflow-y-auto p-8 pt-4 lg:text-lg pb-32"> </textarea>
         </div>
         <div ref="render" class="h-full sm:w-1/2 sm:flex flex-col w-full flex flex-1">
             <ToolBar @toggle="toggle" :view="view" class="sm:hidden" />
-            <div ref="prose" class="flex-1 bg-transparent overflow-y-auto prose max-w-none p-8 pt-4 sm:pr-16 lg:prose-lg rounded-t-2xl lg:prose-pre:max-w-fit" v-html="html" />
+            <div ref="prose" class="flex-1 bg-transparent overflow-y-auto prose max-w-[1000px] p-8 pt-4 sm:pr-16 lg:prose-lg rounded-t-2xl lg:prose-pre:max-w-fit pb-32" v-html="html" />
         </div>
     </div>
 </template>
